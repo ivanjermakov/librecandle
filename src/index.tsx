4ebdc1c10928dbb47671fdeba589800cee0d0c0e
ivanjermakov <ivanjermakov1@gmail.com>
2025-11-06 21:28:48 +0100

    Integrate insight sentry API

--- src/index.tsx
+++ src/index.tsx
@@ -7,42 +7,24 @@ import {
     CrosshairMode,
     IChartApi,
     ISeriesApi,
-    OhlcData,
     Time,
     createChart
 } from 'lightweight-charts'
 import { type Component, onMount } from 'solid-js'
 import './index.css'
 
+const apiKey: string = import.meta.env.INSIGHT_SENTRY_KEY
+/*
+ * https://rapidapi.com/insightsentry-insightsentry-default/api/insightsentry/playground
+ */
+const apiUrl = 'https://insightsentry.p.rapidapi.com/v3'
+
 const App: Component = () => {
     let chartContainer: HTMLDivElement
     let chart: IChartApi
     let ohlcSeries: ISeriesApi<'Candlestick', Time>
 
-    const data: OhlcData[] = [
-        { time: '2018-12-22', open: 75.16, high: 82.84, low: 36.16, close: 45.72 },
-        { time: '2018-12-23', open: 45.12, high: 53.9, low: 45.12, close: 48.09 },
-        { time: '2018-12-24', open: 60.71, high: 60.71, low: 53.39, close: 59.29 },
-        { time: '2018-12-25', open: 68.26, high: 68.26, low: 59.04, close: 60.5 },
-        { time: '2018-12-26', open: 67.71, high: 105.85, low: 66.67, close: 91.04 },
-        { time: '2018-12-27', open: 91.04, high: 121.4, low: 82.7, close: 111.4 },
-        { time: '2018-12-28', open: 111.51, high: 142.83, low: 103.34, close: 131.25 },
-        { time: '2018-12-29', open: 131.33, high: 151.17, low: 77.68, close: 96.43 },
-        { time: '2018-12-30', open: 106.33, high: 110.2, low: 90.39, close: 98.1 },
-        { time: '2018-12-31', open: 109.87, high: 114.69, low: 85.66, close: 111.26 },
-        { time: '2019-01-01', open: 75.16, high: 82.84, low: 36.16, close: 45.72 },
-        { time: '2019-01-02', open: 45.12, high: 53.9, low: 45.12, close: 48.09 },
-        { time: '2019-01-03', open: 60.71, high: 60.71, low: 53.39, close: 59.29 },
-        { time: '2019-01-04', open: 68.26, high: 68.26, low: 59.04, close: 60.5 },
-        { time: '2019-01-05', open: 67.71, high: 105.85, low: 66.67, close: 91.04 },
-        { time: '2019-01-06', open: 91.04, high: 121.4, low: 82.7, close: 111.4 },
-        { time: '2019-01-07', open: 111.51, high: 142.83, low: 103.34, close: 131.25 },
-        { time: '2019-01-08', open: 131.33, high: 151.17, low: 77.68, close: 96.43 },
-        { time: '2019-01-09', open: 106.33, high: 110.2, low: 90.39, close: 98.1 },
-        { time: '2019-01-10', open: 109.87, high: 114.69, low: 85.66, close: 111.26 }
-    ]
-
-    onMount(() => {
+    onMount(async () => {
         chart = createChart(chartContainer!, {
             autoSize: true,
             layout: {
@@ -63,7 +45,9 @@ const App: Component = () => {
                     color: '#222222'
                 }
             },
-            timeScale: {}
+            timeScale: {
+                rightBarStaysOnScroll: true
+            }
         })
 
         const color = {
@@ -77,7 +61,19 @@ const App: Component = () => {
             wickDownColor: color.red,
             borderVisible: false
         })
-
+        const response = await (
+            await fetch(
+                `${apiUrl}/symbols/NASDAQ:AAPL/series?bar_type=day&bar_interval=1&extended=true&badj=true&dadj=false&dp=3000&long_poll=false`,
+                {
+                    headers: {
+                        'x-rapidapi-host': 'insightsentry.p.rapidapi.com',
+                        'x-rapidapi-key': apiKey
+                    }
+                }
+            )
+        ).json()
+        console.log(response)
+        const data = response.series
         ohlcSeries.setData(data)
 
         chart.timeScale().fitContent()
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 
 export default defineConfig({
+    define: {
+        'import.meta.env.INSIGHT_SENTRY_KEY': JSON.stringify(process.env.INSIGHT_SENTRY_KEY)
+    },
     plugins: [solidPlugin()],
     server: {
-        port: 3000
+        port: 3000,
+        hmr: false,
+        watch: undefined
     },
     build: {
         target: 'esnext'
